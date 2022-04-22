b3ffdc3e029834b91e487edf7df912e2c4df6202
Ronit Roy <93121334+ronit100ms@users.noreply.github.com>
2022-04-22 21:19:38 +0530

    Update noiseSuppVirtualBG.test.js

--- src/tests/bottomLeft/noiseSuppVirtualBG.test.js
+++ src/tests/bottomLeft/noiseSuppVirtualBG.test.js
@@ -33,7 +33,7 @@ test(`Verify noise supp and virtual bg visibility`, async ({page}) => {
 })
 
 
-test.only(`Verify noise supp and virtual bg action`, async ({page}) => {
+test(`Verify noise supp and virtual bg action`, async ({page}) => {
   for(let i=0; i<3; i++){
     result = await pageMethods.isElementVisible(page, bottomLeft.virtual_bg_btn, "virtual_bg_btn visibility-")
     pageMethods.assertResult(result, "virtual_bg_btn")
