e19627ec07c5603899ad9c42723dde3d6913747d
ronitroy <ronit@100ms.live>
2022-05-22 23:55:22 +0530

    worker 4 macos

--- src/app/tests/bottomLeft/PlaylistVideo.test.ts
+++ src/app/tests/bottomLeft/PlaylistVideo.test.ts
@@ -2,6 +2,7 @@ import { PageWrapper } from "../../PageWrapper";
 import { test } from "@playwright/test";
 
 let page: PageWrapper;
+let timeout : 2000;
 
 test.beforeEach(async ({ page: nativePage }) => {
   page = await PageWrapper.openMeetingPage(nativePage);
@@ -13,28 +14,26 @@ test.afterEach(async () => {
 });
 
 test(`Playlist Video`, async () => {
-  await page.timeout(500);
-
-  await page.click(
+  await page.clickWithTimeout( timeout,
     page.bottomLeft.video_playlist,
     page.bottomLeft.audio_playlist_item.replace("?", "1")
   );
 
   for (let i = 1; i <= 2; i++) {
-    await page.click(
+    await page.clickWithTimeout( timeout,
       page.bottomLeft.playlist_play_pause_btn,
       page.bottomLeft.playlist_play_pause_btn,
       page.bottomLeft.playlist_next_btn
     );
-    await page.timeout(3000);
+    await page.timeout(2000);
   }
   for (let j = 1; j <= 2; j++) {
-    await page.click(
+    await page.clickWithTimeout( timeout,
       page.bottomLeft.playlist_play_pause_btn,
       page.bottomLeft.playlist_play_pause_btn,
       page.bottomLeft.playlist_prev_btn
     );
-    await page.timeout(3000);
+    await page.timeout(2000);
   }
   await page.click(page.bottomLeft.videoplayer_cross_btn);
 });
