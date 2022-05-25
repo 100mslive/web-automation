a9daf105504e44fa0ea56b01fcd2ebaabce35d51
Tushar <tushar@100ms.live>
2022-05-25 22:53:27 +0530

    ci: use handlebars for slack message templating

--- src/slackReporter.ts
+++ src/slackReporter.ts
@@ -5,9 +5,65 @@ import {
   Suite,
   TestCase,
   TestResult,
+  TestStatus,
 } from "@playwright/test/reporter";
 import { writeFileSync } from "fs";
-const github = require("@actions/github");
+import * as HandleBars from "handlebars";
+
+class slackReporter implements Reporter {
+  private message = "";
+  private counters: Record<TestStatus, number> = { failed: 0, passed: 0, skipped: 0, timedOut: 0 };
+  private failures = "";
+
+  onBegin(config: FullConfig, suite: Suite): void {
+    this.addCountToMessage("Total", suite.allTests().length);
+  }
+
+  onTestEnd(test: TestCase, result: TestResult): void {
+    this.counters[result.status]++;
+    if (result.status === "failed" || result.status === "timedOut") {
+      this.failures += `${test.title} \n`;
+    }
+    console.log(`Finished test ${test.title}: ${result.status}`);
+  }
+
+  onEnd(result: FullResult): void {
+    this.addCountToMessage("Passed", this.counters.passed);
+    this.addCountToMessage("Failed", this.counters.failed);
+    this.addCountToMessage("Timed Out", this.counters.timedOut);
+    this.addCountToMessage("Skipped", this.counters.skipped);
+
+    if (result.status === "failed") {
+      slackPayload.attachments[0].color = "#af0e20";
+      this.message += "\n*Failed Tests - *\n";
+      this.message += this.failures;
+    }
+
+    console.log(`Finished the run: ${result.status}`);
+    slackText.text = this.message;
+    let slackJson = JSON.stringify(slackPayload, null, 2);
+    const emoji = result.status === "passed" ? ":tada:" : ":cry:";
+    slackJson = this.interpolateSlackTemplate(slackJson, { jobStatus: result.status, emoji });
+    console.log("slack message payload", slackJson);
+    writeFileSync("slackMessage.json", slackJson);
+  }
+
+  interpolateSlackTemplate(payload: string, variables: Record<string, string>) {
+    payload = payload.replaceAll("$", "");
+    const context = {
+      env: process.env,
+      ...variables,
+    };
+    const template = HandleBars.compile(payload);
+    return template(context);
+  }
+
+  addCountToMessage(label: string, count: number) {
+    if (count) {
+      this.message += `*${label}:*  ${count}\n`;
+    }
+  }
+}
 
 const slackText = {
   type: "mrkdwn",
@@ -23,7 +79,7 @@ const slackPayload = {
           type: "section",
           text: {
             type: "mrkdwn",
-            text: "Web-Automation build result for ${{github.env.REGION}}: ${{github.job.status}} - ${{github.actor}}",
+            text: "*Web-Automation results for ${{env.REGION}}: ${{jobStatus}} ${{emoji}}*",
           },
         },
         {
@@ -40,10 +96,19 @@ const slackPayload = {
               type: "button",
               text: {
                 type: "plain_text",
+                text: "Test Run",
+                emoji: true,
+              },
+              url: "${{env.REPO_URL}}/actions/runs/${{env.GITHUB_RUN_ID}}",
+            },
+            {
+              type: "button",
+              text: {
+                type: "plain_text",
                 text: "Results",
                 emoji: true,
               },
-              url: "https://web-automation-git-${{github.env.REGION}}-100mslive.vercel.app/",
+              url: "https://web-automation-git-${{env.REGION}}-100mslive.vercel.app/",
             },
           ],
         },
@@ -52,51 +117,4 @@ const slackPayload = {
   ],
 };
 
-console.log("github context", github.context);
-
-let message = "";
-let passedtest = "";
-let failedtest = "";
-class slackReporter implements Reporter {
-  onBegin(config: FullConfig, suite: Suite): void {
-    message += `Running Automation on ${process.env.APP_ENV} \nTotal tests:  ${
-      suite.allTests().length
-    }`;
-  }
-
-  onTestBegin(test: TestCase): void {
-    console.log(`Starting test ${test.title}`);
-  }
-
-  //   onStdOut(chunk: string|Buffer, test: void|TestCase, result: void|TestResult): void{
-  //     console.log(chunk);
-  //   }
-
-  onTestEnd(test: TestCase, result: TestResult): void {
-    if (result.status === "failed") {
-      failedtest += `${test.title} \n`;
-    }
-    if (result.status === "passed") {
-      passedtest += `${test.title} \n`;
-    }
-    console.log(`Finished test ${test.title}: ${result.status}`);
-  }
-
-  onEnd(result: FullResult): void {
-    if (result.status === "failed") {
-      message += `\nFailed --> ${failedtest.length}`;
-      message += `\nFailed tests: \n${failedtest.length > 0 ? failedtest : "None"}`;
-      slackPayload.attachments[0].color = "#af0e20";
-    }
-    if (result.status === "passed") {
-      message += `\nPassed --> ${passedtest}`;
-    }
-
-    console.log(`Finished the run: ${result.status}`);
-    slackText.text = message;
-    const slackJson = JSON.stringify(slackPayload, null, 2);
-    console.log(slackJson);
-    writeFileSync("slackMessage.json", slackJson);
-  }
-}
 export default slackReporter;
