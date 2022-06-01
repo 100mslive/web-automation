4517bfe3ad854d4b8f43058f1107433013a49b3d
ronitroy <ronit@100ms.live>
2022-06-01 16:16:26 +0530

    feat: Remote Mute/Unmute Audio Video

--- src/app/tests/Superpowers/RemoteMute.test.ts
+++ src/app/tests/Superpowers/RemoteMute.test.ts
@@ -26,25 +26,25 @@ test(`Remote Mute/Unmute Audio`, async ({ context }) => {
 });
 
 test(`Remote Mute/Unmute Video`, async ({ context }) => {
-    const pages = await PageWrapper.openPages(context, 2, {
-      mic: true,
-      cam: true,
-    });
-  
-    await pages[0].hover(pages[0].center.participant_tile.replace("?", pages[1].localName));
-    await pages[0].click(pages[0].center.participant_tile_menu_btn.replace("?", pages[1].localName));
-    await pages[0].click(pages[0].center.tile_menu_mute_video);
-    await pages[1].assertLocalVideoState(false);
+  const pages = await PageWrapper.openPages(context, 2, {
+    mic: true,
+    cam: true,
+  });
 
-    // await pages[0].hover(pages[0].center.participant_tile.replace("?", pages[1].localName));
-    // await pages[0].click(pages[0].center.participant_tile_menu_btn.replace("?", pages[1].localName));
-    await pages[0].click(pages[0].center.tile_menu_unmute_video);
+  await pages[0].hover(pages[0].center.participant_tile.replace("?", pages[1].localName));
+  await pages[0].click(pages[0].center.participant_tile_menu_btn.replace("?", pages[1].localName));
+  await pages[0].click(pages[0].center.tile_menu_mute_video);
+  await pages[1].assertLocalVideoState(false);
+
+  // await pages[0].hover(pages[0].center.participant_tile.replace("?", pages[1].localName));
+  // await pages[0].click(pages[0].center.participant_tile_menu_btn.replace("?", pages[1].localName));
+  await pages[0].click(pages[0].center.tile_menu_unmute_video);
   await pages[1].clickWithTimeout(2000, pages[0].center.dialog_accept);
 
-    // await pages[1].acceptDialogWhenPrompted();
-    await pages[1].assertLocalVideoState(true);
-  
-    //add more verify
-    await pages[0].endRoom();
-    await context.close();
-  });
+  // await pages[1].acceptDialogWhenPrompted();
+  await pages[1].assertLocalVideoState(true);
+
+  //add more verify
+  await pages[0].endRoom();
+  await context.close();
+});
