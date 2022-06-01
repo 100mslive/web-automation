20ff49c1239f25a4644ea5cdf84fc33d9e3fb7c2
ronitroy <ronit@100ms.live>
2022-06-01 17:32:50 +0530

    feat: Remote Mute/Unmute Audio Video

--- src/app/tests/Superpowers/RemoteMute.test.ts
+++ src/app/tests/Superpowers/RemoteMute.test.ts
@@ -5,7 +5,7 @@ test.beforeEach(async () => {});
 
 test.afterEach(async () => {});
 
-test(`Remote Mute/Unmute Audio`, async ({ context }) => {
+test.only(`Remote Mute/Unmute Audio`, async ({ context }) => {
   const pages = await PageWrapper.openPages(context, 2, {
     mic: true,
     cam: true,
@@ -17,7 +17,8 @@ test(`Remote Mute/Unmute Audio`, async ({ context }) => {
   await pages[1].assertLocalAudioState(false);
 
   await pages[0].click(pages[0].center.tile_menu_mute_audio);
-  await pages[1].clickWithTimeout(2000, pages[0].center.dialog_accept);
+  await pages[1].timeout(2000);
+  await pages[1].click(pages[0].center.dialog_accept);
   await pages[1].assertLocalAudioState(true);
 
   //add more verify
@@ -25,7 +26,7 @@ test(`Remote Mute/Unmute Audio`, async ({ context }) => {
   await context.close();
 });
 
-test(`Remote Mute/Unmute Video`, async ({ context }) => {
+test.only(`Remote Mute/Unmute Video`, async ({ context }) => {
   const pages = await PageWrapper.openPages(context, 2, {
     mic: true,
     cam: true,
@@ -36,12 +37,9 @@ test(`Remote Mute/Unmute Video`, async ({ context }) => {
   await pages[0].click(pages[0].center.tile_menu_mute_video);
   await pages[1].assertLocalVideoState(false);
 
-  // await pages[0].hover(pages[0].center.participant_tile.replace("?", pages[1].localName));
-  // await pages[0].click(pages[0].center.participant_tile_menu_btn.replace("?", pages[1].localName));
   await pages[0].click(pages[0].center.tile_menu_unmute_video);
-  await pages[1].clickWithTimeout(2000, pages[0].center.dialog_accept);
-
-  // await pages[1].acceptDialogWhenPrompted();
+  await pages[1].timeout(2000);
+  await pages[1].click(pages[0].center.dialog_accept);
   await pages[1].assertLocalVideoState(true);
 
   //add more verify
