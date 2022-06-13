aaa29fad7b073fa3ba2b0733f8c4eeac495ab9b6
sahil-100ms <105665560+sahil-100ms@users.noreply.github.com>
2022-06-13 16:09:04 +0530

    Update PreviewPageEmulation.test.ts

--- src/app/tests/network/PreviewPageEmulation.test.ts
+++ src/app/tests/network/PreviewPageEmulation.test.ts
@@ -17,7 +17,6 @@ test(`Verify you are offline now notification`, async () => {
   await page.assertVisible(page.preview.preview_audio_on_btn);
   await page.emulateNetwork(true, -1, -1, -1);
   await page.assertVisible(page.center.network_offline_notification);
-  await page.close();
 });
 
 test(`Verify you are now connected notification`, async () => {
@@ -26,5 +25,4 @@ test(`Verify you are now connected notification`, async () => {
     await page.assertVisible(page.center.network_offline_notification);
     await page.emulateNetwork(false, 0, 500, 500);
     await page.assertVisible(page.center.network_connected_notification);
-    await page.close();
 });
